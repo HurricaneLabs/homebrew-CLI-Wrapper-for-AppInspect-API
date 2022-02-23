fab863d27151db1bf54f1ff30564d38c81ea903d
deviansg <iangillespie1982@gmail.com>
2022-02-23 11:46:09 -0500

    Updated SHA
    Rebuilt v0.1.9

--- cli-wrapper-for-appinspect-api.rb
+++ cli-wrapper-for-appinspect-api.rb
@@ -5,7 +5,7 @@ class CliWrapperForAppinspectApi < Formula
   desc "CLI wrapper for AppInspect API"
   homepage "https://github.com/HurricaneLabs/CLI-Wrapper-for-AppInspect-API"
   url "https://github.com/HurricaneLabs/CLI-Wrapper-for-AppInspect-API/releases/download/v0.1.9/macos-appinspect.tar.gz"
-  sha256 "17c556306a49eaa501a5817abd604c58c2a364f533e809ebc3d637bb8ec37e20"
+  sha256 "63fd81d528cc6fe0bf0e56f16d255d8c27fa566f3959294a9e88723c9f429fdf"
   license "MIT"
 
   def install
